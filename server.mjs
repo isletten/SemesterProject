a93194f37c21f7029f673ce83fa95b9b951b3dda
isletten <ingrid.a.sletten@gmail.com>
2024-03-05 14:24:00 +0100

    oppdatert med nye filer

--- server.mjs
+++ server.mjs
@@ -1,12 +1,18 @@
+import 'dotenv/config'
 import express from 'express' // Express is installed using npm
-import USER_API from './routes/usersRoute.mjs'; // This is where we have defined the API for working with users.
-
+import USER_API from './routes/usersRoute.mjs'; // This is where we have defined the API for working with users
 import SuperLogger from './modules/SuperLogger.mjs';
+import printDeveloperStartupInportantInformationMSG from "./modules/developerHelpers.mjs";
+
+printDeveloperStartupInportantInformationMSG();
+
+
 // Creating an instance of the server
 const server = express();
 // Selecting a port for the server to use.
 const port = (process.env.PORT || 8080);
 server.set('port', port);
+
 
 // Enable logging for server
 const logger = new SuperLogger();
@@ -21,11 +27,10 @@ server.use("/user", USER_API);
 
 // A get request handler example)
 server.get("/", (req, res, next) => {
-
-    req.originalUrl
-
     res.status(200).send(JSON.stringify({ msg: "These are not the droids...." })).end();
 });
+
+
 
 // Start the server 
 server.listen(server.get('port'), function () {
